580d862c55efb93267039b4d908f466c988f974d
md tarek hossain <hossaintarek944@gmail.com>
2023-08-15 11:17:15 +0600

    array

---
+++ arrayMax.js
@@ -0,0 +1,19 @@
+const names = 'Tarek hossain'
+const age = 24;
+const number = [78,46, 35, 67, 345, 45, 45];
+const number1 = [78,46, 35, 67, 345, 45, 45];
+console.log(typeof(names));
+console.log(typeof number)
+console.log(Array.isArray(number))
+// console.log(Array.isArray number)
+if(number.indexOf !== -1){
+    console.log('This is not an array')
+}
+else{
+    console.log('This is an array')
+}
+// concat.....................
+console.log(number.concat(number1))
+const paglu = number.concat(number1);
+const paglu2 = paglu.join();
+console.log(paglu2);
