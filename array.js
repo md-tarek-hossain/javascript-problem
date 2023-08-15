7a2c2be1f71666354cc04d343feddc282d0ee6c5
md tarek hossain <hossaintarek944@gmail.com>
2023-08-15 10:30:41 +0600

    java soluation

---
+++ array.js
@@ -0,0 +1,35 @@
+// var friends =[23, 342 , 53 ,]
+// var me ='Tarek hossain'
+// var age = 24;
+// var isHeMarrid = true;
+
+// console.log(Array.isArray(friends))
+// console.log(typeof friends)
+// console.log(typeof me)
+// console.log(typeof age)
+// console.log(typeof isHeMarrid)
+// // .........................
+
+// console.log(friends.includes(232))
+// console.log(friends.includes(53))
+// if(friends.indexOf(342) !== -1){
+//     console.log('Ho mama, acetoh!!')
+// }
+// else{ 
+//     console.log('na mama, nai!!')
+// }
+// // var friends =[23, 342 , 53 ,]
+// // var newFriend=[34,  45, 56, 76, 58]
+// // var allFriends = friends.concat(newFriend);
+// // console.log(allFriends)
+
+let fruits = ['Banana', 'Apple', 'Orange'];
+let moreFruits = ['Finaple', 'Graps'];
+// let allFruits = moreFruits.concat(fruits);
+// console.log(allFruits)
+// let connect = fruits.join();
+// console.log(connect);
+let joinArray = fruits.concat(moreFruits);
+console.log(joinArray);
+let connection = joinArray.join()
+console.log(connection);
